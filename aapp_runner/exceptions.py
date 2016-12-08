303368b5f9cc20e256d42789ce15c14478137ad4
Trygve Aspenes <Trygve.Aspenes@met.no>
2016-12-08 09:01:24 +0100

    Added exceptions classes for specific aapp processing failures

---
+++ aapp_runner/exceptions.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016
+#
+# Author(s):
+#
+#   Trygve Aspenes <trygveas@metno>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from libtiff.libtiff_ctypes import value
+
+'''Exceptions needed for aapp runner
+'''
+
+class TleError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+    
+class SatposError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class DecommutationError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
