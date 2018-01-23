78d7c4dd96ba0a93f972056603c04559b428f83f
Trygve Aspenes <trygve.aspenes@met.no>
2018-01-23 11:43:58 +0000

    Only show renamed files if any

--- aapp_runner/rename_aapp_filenames.py
+++ aapp_runner/rename_aapp_filenames.py
@@ -130,5 +130,6 @@ def rename_aapp_filenames(process_config):
     #Change back after this is done
     os.chdir(current_dir)
 
-    LOG.info("Renamed aapp files complete into: {}!".format(os.path.dirname(files[0]['file'])))
+    if len(files) > 0:
+        LOG.info("Renamed aapp files complete into: {}!".format(os.path.dirname(files[0]['file'])))
     return files
