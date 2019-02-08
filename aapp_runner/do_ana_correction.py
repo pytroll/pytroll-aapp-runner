d6e58454f3b7006591d793653c1ceb4acb1ea652
Trygve Aspenes <trygveas@met.no>
2019-02-08 16:14:03 +0100

    flake8 adjust,enst

--- aapp_runner/do_ana_correction.py
+++ aapp_runner/do_ana_correction.py
@@ -32,17 +32,18 @@ from helper_functions import run_shell_command
 
 LOG = logging.getLogger(__name__)
 
+
 def do_ana_correction(process_config, msg, timestamp):
 
     """The software ANA is 3party. User will have to get it themself.
     Further the ana binaries and scripts must be in the PATH,
     preferable in the primary AAPP bin directory
-   
-    Also it relays on reference_landmarks directory under ENV(DIR_NAVIGATION)/ana dir
-    This needs to be copied/installed here by the user 
 
-    TODO: Replace the ana_lmk_loc script with code here to better be able to control 
-    the directories used in the processing 
+    Also it relays on reference_landmarks directory under ENV(DIR_NAVIGATION)/ana dir
+    This needs to be copied/installed here by the user
+
+    TODO: Replace the ana_lmk_loc script with code here to better be able to control
+    the directories used in the processing
     """
 
     if not process_config['process_avhrr']:
@@ -53,17 +54,17 @@ def do_ana_correction(process_config, msg, timestamp):
         if 'do_ana_correction' in process_config['aapp_processes'][process_config.process_name]:
             if not process_config['aapp_processes'][process_config.process_name]['do_ana_correction']:
                 return True
-    except Exception, err:
+    except Exception as err:
         LOG.error("Failed with: {}".format(err))
 
     return_status = True
-    
-    #This function relays on beeing in a working directory
-    current_dir = os.getcwd() #Store the dir to change back to after function complete
+
+    # This function relays on beeing in a working directory
+    current_dir = os.getcwd()  # Store the dir to change back to after function complete
     os.chdir(process_config['aapp_processes'][process_config.process_name]['working_dir'])
 
-    #Must check of the ana dir exists
-    ana_dir = os.path.join(os.getenv('DIR_NAVIGATION'),'ana')
+    # Must check of the ana dir exists
+    ana_dir = os.path.join(os.getenv('DIR_NAVIGATION'), 'ana')
     if not os.path.exists(ana_dir):
         try:
             os.makedirs(ana_dir)
@@ -72,115 +73,130 @@ def do_ana_correction(process_config, msg, timestamp):
             LOG.error("Failed to create directory: {}. This is needed to run the ANA software.")
             return_status = False
 
-    #And the ana dir must contain a reference_landmarks directory to work!
-    if not os.path.exists(os.path.join(ana_dir,"reference_landmarks")):
+    # And the ana dir must contain a reference_landmarks directory to work!
+    if not os.path.exists(os.path.join(ana_dir, "reference_landmarks")):
         LOG.error("Can not run ANA because reference_landmarks under the ana dir is missing.")
         return_status = False
-        
+
     if return_status:
-        #Find all matching landmarks
+        # Find all matching landmarks
         cmd = "ana_lmk_loc -D {}".format(process_config['aapp_static_configuration']['decommutation_files']['avhrr_file'])
         try:
             status = False
-            status, returncode, std, err = run_shell_command(cmd, stdout_logfile="ana_lmk_loc.log",stderr_logfile="ana_lmk_loc.err")
+            status, returncode, std, err = run_shell_command(cmd,
+                                                             stdout_logfile="ana_lmk_loc.log",
+                                                             stderr_logfile="ana_lmk_loc.err")
         except:
             import sys
-            LOG.error("Command {} failed with {}.".format(cmd,sys.exc_info()[0]))
+            LOG.error("Command {} failed with {}.".format(cmd, sys.exc_info()[0]))
             if not status:
                 return_status = False
         else:
             if not status or returncode != 0:
                 LOG.error("Command {} failed with {}".format(cmd, returncode))
-                _ana_file = open('ana_lmk_loc.err',"w")
+                _ana_file = open('ana_lmk_loc.err', "w")
                 _ana_file.write(std)
                 _ana_file.write(err)
                 _ana_file.close()
                 return_status = False
 
     if return_status:
-        #Check of ana landmark location file with correct time stamp exist. If not try to find correct timestamp and copy to this. 
-        expected_ana_loc_file = "lmkloc_{}_{:%Y%m%d_%H%M}_{:05d}.txt".format(process_config['platform_name'], timestamp, process_config['orbit_number'])
-        if not os.path.exists(os.path.join(ana_dir,"{:%Y-%m}".format(timestamp),expected_ana_loc_file)):
-            #Need to run this AAPP command to detect the start of the hrpt.l1b(avhrr data) file
-            #Because this might differ from the time stamp in file name
+        # Check of ana landmark location file with correct time stamp exist.
+        # If not try to find correct timestamp and copy to this.
+        expected_ana_loc_file = "lmkloc_{}_{:%Y%m%d_%H%M}_{:05d}.txt".format(process_config['platform_name'],
+                                                                             timestamp,
+                                                                             process_config['orbit_number'])
+        if not os.path.exists(os.path.join(ana_dir, "{:%Y-%m}".format(timestamp), expected_ana_loc_file)):
+            # Need to run this AAPP command to detect the start of the hrpt.l1b(avhrr data) file
+            # Because this might differ from the time stamp in file name
             cmd = "l1bidf.exe"
             try:
-                status, returncode, std, err = run_shell_command(cmd,stdin="{}\n".format(process_config['aapp_static_configuration']['decommutation_files']['avhrr_file']))
+                status, returncode, std, err = run_shell_command(cmd,
+                                                                 stdin="{}\n".format(
+                                                                     process_config['aapp_static_configuration']
+                                                                     ['decommutation_files']['avhrr_file']))
             except:
                 import sys
-                LOG.error("Command {} failed with {}.".format(cmd,sys.exc_info()[0]))
+                LOG.error("Command {} failed with {}.".format(cmd, sys.exc_info()[0]))
                 if not status:
                     return_status = False
             else:
                 if not status or returncode != 0:
                     LOG.error("Command {} failed with {}".format(cmd, returncode))
-                    _ana_file = open('ana_lmk_loc.err',"w")
+                    _ana_file = open('ana_lmk_loc.err', "w")
                     _ana_file.write(std)
                     _ana_file.write(err)
                     _ana_file.close()
                     return_status = False
                 else:
                     l1bidf_list = std.split()
-                    l1bidf_aapp_datetime = datetime.strptime(l1bidf_list[1]+l1bidf_list[2],"%Y%m%d%H%M")
+                    l1bidf_aapp_datetime = datetime.strptime(l1bidf_list[1] + l1bidf_list[2], "%Y%m%d%H%M")
                     LOG.debug("l1b idf aapp datetime: {}".format(l1bidf_aapp_datetime))
-                    #estatt_file_name =     lmkloc_metop02_20160617_0952_50132.txt
-                    ana_loc_file = "lmkloc_{}_{:%Y%m%d_%H%M}_{:05d}.txt".format(l1bidf_list[0], l1bidf_aapp_datetime, int(l1bidf_list[3]))
-                    if os.path.exists(os.path.join(ana_dir,"{:%Y-%m}".format(l1bidf_aapp_datetime),ana_loc_file)):
+                    # estatt_file_name =     lmkloc_metop02_20160617_0952_50132.txt
+                    ana_loc_file = "lmkloc_{}_{:%Y%m%d_%H%M}_{:05d}.txt".format(l1bidf_list[0],
+                                                                                l1bidf_aapp_datetime,
+                                                                                int(l1bidf_list[3]))
+                    if os.path.exists(os.path.join(ana_dir, "{:%Y-%m}".format(l1bidf_aapp_datetime), ana_loc_file)):
                         LOG.debug("timestamp {} l1bidf_aapp_datetime: {}".format(timestamp, l1bidf_aapp_datetime))
                         from shutil import copy2
-                        copy2(os.path.join(ana_dir,"{:%Y-%m}".format(timestamp),ana_loc_file),
-                              os.path.join(ana_dir,"{:%Y-%m}".format(l1bidf_aapp_datetime),expected_ana_loc_file))
+                        copy2(os.path.join(ana_dir, "{:%Y-%m}".format(timestamp), ana_loc_file),
+                              os.path.join(ana_dir, "{:%Y-%m}".format(l1bidf_aapp_datetime), expected_ana_loc_file))
                     else:
                         LOG.warning("Could not find ana loc file: {}".format(ana_loc_file))
         else:
-            LOG.debug("File ok: {}".format(os.path.join(ana_dir,"{:%Y-%m}".format(timestamp),expected_ana_loc_file)))
-        
+            LOG.debug("File ok: {}".format(os.path.join(ana_dir, "{:%Y-%m}".format(timestamp), expected_ana_loc_file)))
+
     import hashlib
     if return_status:
-        #LOG.debug("sha256 of aapp input avhrr_file: {}".format(hashlib.sha256(open(process_config['aapp_static_configuration']['decommutation_files']['avhrr_file'], 'rb').read()).hexdigest()))
-    
-        #Calculate correction from landmarks and update avhrr_file with these new attitude coefisients.
-        cmd = "ana_estatt -s {0} -d {1:%Y%m%d} -h {1:%H%M} -n {2:05d}".format(process_config['platform_name'], timestamp, process_config['orbit_number'])
+        # LOG.debug("sha256 of aapp input avhrr_file: {}".format(hashlib.sha256(open(
+        # process_config['aapp_static_configuration']['decommutation_files']['avhrr_file'], 'rb').read()).hexdigest()))
+
+        # Calculate correction from landmarks and update avhrr_file with these new attitude coefisients.
+        cmd = "ana_estatt -s {0} -d {1:%Y%m%d} -h {1:%H%M} -n {2:05d}".format(process_config['platform_name'],
+                                                                              timestamp,
+                                                                              process_config['orbit_number'])
         try:
             status, returncode, std, err = run_shell_command(cmd)
         except:
             import sys
-            LOG.error("Command {} failed with {}.".format(cmd,sys.exc_info()[0]))
+            LOG.error("Command {} failed with {}.".format(cmd, sys.exc_info()[0]))
             if not status:
                 return_status = False
         else:
             if not status or returncode != 0:
                 LOG.error("Command {} failed with {}".format(cmd, returncode))
-                _ana_file = open('ana_lmk_loc.err',"w")
+                _ana_file = open('ana_lmk_loc.err', "w")
                 _ana_file.write(std)
                 _ana_file.write(err)
                 _ana_file.close()
                 return_status = False
-    
-        #LOG.debug("sha256 of aapp input avhrr_file: {}".format(hashlib.sha256(open(process_config['aapp_static_configuration']['decommutation_files']['avhrr_file'], 'rb').read()).hexdigest()))
-    
-        import hashlib
+
+        # LOG.debug("sha256 of aapp input avhrr_file: {}".format(hashlib.sha256(open(
+        # process_config['aapp_static_configuration']['decommutation_files']['avhrr_file'], 'rb').read()).hexdigest()))
+
         sha256_before_correction = hashlib.sha256(open(process_config['aapp_static_configuration']['decommutation_files']['avhrr_file'], 'rb').read()).hexdigest()
 
     if return_status:
-        #Recalculate the location in the avhhr data file with the new correction attitude coefisients.
+        # Recalculate the location in the avhhr data file with the new correction attitude coefisients.
         from do_avhrr_calibration import do_avhrr_calibration
         LOG.info("Need to recalculate the avhrcl with new attitude coefisients from ANA.")
         if not do_avhrr_calibration(process_config, msg, timestamp):
-            LOG.warning("The avhrr location with ana correction failed for some reason. It might be that the processing can continue")
+            LOG.warning("The avhrr location with ana correction failed for some reason."
+                        "It might be that the processing can continue")
             LOG.warning("Please check the previous log carefully to see if this is an error you can accept.")
             return_status = False
-            
+
     if return_status:
         sha256_after_correction = hashlib.sha256(open(process_config['aapp_static_configuration']['decommutation_files']['avhrr_file'], 'rb').read()).hexdigest()
         LOG.debug("sha256 of aapp input avhrr_file BEFORE ana: {}".format(sha256_before_correction))
         LOG.debug("sha256 of aapp input avhrr_file AFTER  ana: {}".format(sha256_after_correction))
-        if ( sha256_before_correction == sha256_after_correction):
-            LOG.warning("The correction of the avhrr location with data from ANA did not take place for some reason, or the correction was 0 for all pitch yaw and roll.")
+        if (sha256_before_correction == sha256_after_correction):
+            LOG.warning("The correction of the avhrr location with data from ANA did not take place for some reason,"
+                        "or the correction was 0 for all pitch yaw and roll.")
         else:
             LOG.info("The correction of AVHRR location with data from ANA was performed.")
-    
-    #Change back after this is done
+
+    # Change back after this is done
     os.chdir(current_dir)
 
     LOG.info("do_ana_correction complete!")
