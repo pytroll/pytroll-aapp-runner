957b6122e35cfa2b0f81a68d84d44a395eab6ad2
Adam Dybbroe <adybbroe@users.noreply.github.com>
2021-03-11 19:27:34 +0100

    Update bin/aapp_dr_runner.py
    Co-authored-by: Martin Raspaud <martin.raspaud@smhi.se>

--- bin/aapp_dr_runner.py
+++ bin/aapp_dr_runner.py
@@ -195,7 +195,8 @@ def cleanup_aapp_workdir(config):
     try:
         filelist = glob(
             '%s/*' % config['aapp_processes'][config.process_name]['working_dir'])
-        _ = [os.remove(s) for s in filelist if os.path.isfile(s)]
+        for s in filelist if os.path.isfile(s):
+            os.remove(s)
         shutil.rmtree(
             config['aapp_processes'][config.process_name]['working_dir'])
     except Exception as err:
